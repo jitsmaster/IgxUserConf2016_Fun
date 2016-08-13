a618f6a7537a0b26f035aaa578e66b692a06d4c6
Arnold Wang <arnold.wang@ingeniux.com>
2016-08-13 12:36:02 -0700

    Loading indicator becomes a progress bar

--- src/loading-indicator.component.ts
+++ src/loading-indicator.component.ts
@@ -3,19 +3,17 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'loading-indicator',
   template: `
-    <div [style.width]="width" [style.height]="height" class="progressPx"></div>
+    <div [style.width]="progressPx" [style.top]="top" class="progress"></div>
   `,
   styles: [require('./loading-indicator.component.css').toString()]
 })
 export class LoadingIndicator {
   @Input() progress = 0;
 
-  width: string;
-  height: string;
+  top: string;
 
   ngOnInit() {
-    this.width = window.screen.width + "px";
-    this.height = window.screen.height * 0.9 + "px";
+    this.top = window.screen.height * 0.8 + "px";
   }
 
   get progressPx(): string {
