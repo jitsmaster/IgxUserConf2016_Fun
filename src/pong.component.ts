da1bbba287051e0a921b3fe45d52abc9762df542
Arnold Wang <awang@ingeniux.com>
2016-09-15 08:36:37 -0700

    settings options and difficulty adjustment in place Still on change detection

--- src/pong.component.ts
+++ src/pong.component.ts
@@ -1,13 +1,13 @@
 import {Component, Inject, Injector, Input, OnInit, OnChanges, OnDestroy, ViewChild, ChangeDetectionStrategy,
 	ElementRef,
-	trigger, transition, animate, style, group} from '@angular/core';
+	trigger, transition, animate, style, state, group} from '@angular/core';
 import {DOCUMENT} from '@angular/platform-browser';
 import {Samples} from './services/samples.service';
 import {Audio} from './services/audio.service';
 import {Observable, BehaviorSubject} from 'rxjs/Rx';
 
-const PADDLE_MOVE_SPEED: number = 8;
-const BALL_SPEED: number = 5;
+const PADDLE_MOVE_SPEED: number = 5;
+const BALL_SPEED: number = 4;
 const ANGLE = 45; //@45 degrees
 const BOTTOM_OFFSET = 100;
 
@@ -19,18 +19,45 @@ class Pos {
 @Component({
 	selector: 'pong-game',
 	template: `
-		<div class="scoreCard">
-			Score: <span [innerHTML]="score | async"></span>
-		</div>
-		<div #paddle class="paddle" [style.top]="paddlePosition | async"></div>
-		<div #ball class="ball" [style.left.px]="(ballPosition | async).x"
-			[style.top.px]="(ballPosition | async).y"></div>
+	<label>
+      <input type='checkbox' (change)='toggleSettings()'/>  
+      Settings
+    </label>
+    <div [@configSlide]="showAdvanced ? 'open' : 'closed'">
+      <span class='config'>
+        Difficulty:
+        <select [(ngModel)]='difficulty'>
+          <option value="1">Just close my eyes</option>
+          <option value="2">Spend most time waitinng</option>
+          <option value="3" selected >My old fond childhood memory</option>
+          <option value="4">Rocky catching a chicken</option>
+          <option value="5">Chased by a thousand bees</option>
+		  <option value="6">Dark Souls, Bloodborne, Nioh...</option>
+        </select>
+      </span>
+    </div>
+	<div class="scoreCard">
+		Score: <span [innerHTML]="score | async"></span>
+	</div>
+	<div #paddle class="paddle" [style.top]="paddlePosition | async"></div>
+	<div #ball class="ball" [style.left.px]="(ballPosition | async).x"
+		[style.top.px]="(ballPosition | async).y"></div>
 	`,
 	styles: [require('./pong.component.css').toString()],
 	host: {
 		'(document:keydown)': 'movePaddle($event)',
 		'(document:keyup)': 'paddleStop($event)'
 	},
+	animations: [
+		trigger('configSlide', [
+			state('void', style({ height: 0, transform: "translate3d(-100% ,0 ,0)" })),
+			state('closed', style({ height: 0, transform: "translate3d(-100% ,0 ,0)" })),
+			state('open', style({ height: '*', transform: "translate3d(0 ,0 ,0)" })),
+			transition('closed => open', [
+				animate('600ms cubic-bezier(0.3, 0, 0.1, 1.7)')]),
+			transition('open => closed', [animate('400ms ease-out')])
+		])
+	],
 	changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Pong implements OnDestroy, OnInit {
@@ -57,8 +84,8 @@ export class Pong implements OnDestroy, OnInit {
 	private _ballMoveTimer;
 	private _scoreTimer;
 
-	private _paddleSpeedMultiplier: number = 1;
-	private _ballSpeedMultiplier: number = 2;
+	private _paddleSpeedMultiplier: number = 3;
+	private _ballSpeedMultiplier: number = 3;
 	private _tanMultiplier: number = 1;
 
 	get paddleSpeed() {
@@ -71,6 +98,53 @@ export class Pong implements OnDestroy, OnInit {
 
 	get angle() {
 		return ANGLE * this._tanMultiplier;
+	}
+
+	_difficulty: string = '3';
+
+	get difficulty(): string {
+		return this._difficulty;
+	}
+
+	set difficulty(val: string) {
+		this._difficulty = val;
+		switch (val) {
+			case '1':
+				this._paddleSpeedMultiplier = 2;
+				this._ballSpeedMultiplier = 1;
+				this._tanMultiplier = 1;
+				break;
+			case '2':
+				this._paddleSpeedMultiplier = 2;
+				this._ballSpeedMultiplier = 2;
+				this._tanMultiplier = 1;
+				break;
+			case '4':
+				this._paddleSpeedMultiplier = 4;
+				this._ballSpeedMultiplier = 4;
+				this._tanMultiplier = 1;
+				break;
+			case '5':
+				this._paddleSpeedMultiplier = 5;
+				this._ballSpeedMultiplier = 8;
+				this._tanMultiplier = 1;
+				break;
+			case '6':
+				this._paddleSpeedMultiplier = 6;
+				this._ballSpeedMultiplier = 8;
+				this._tanMultiplier = Math.max(0.1, Math.random());
+				break;
+			default:
+				this._paddleSpeedMultiplier = 3;
+				this._ballSpeedMultiplier = 3;
+				this._tanMultiplier = 1;
+		}
+	}
+
+	showAdvanced: boolean;
+
+	toggleSettings() {
+		this.showAdvanced = !this.showAdvanced;
 	}
 
 	private _xRevert: boolean;
@@ -152,7 +226,7 @@ export class Pong implements OnDestroy, OnInit {
 	movePaddle(evt: KeyboardEvent) {
 
 		//add 100 offset for bottom
-		this._paddleHeight = this.paddle.nativeElement.offsetHeight - BOTTOM_OFFSET;
+		this._paddleHeight = this.paddle.nativeElement.clientHeight;
 
 		this.paddleStop(null);
 
@@ -178,7 +252,8 @@ export class Pong implements OnDestroy, OnInit {
 		else if (keyCode == 40) {
 			//down
 			this._paddlePos += this.paddleSpeed;
-			this._paddlePos = Math.min(window.screen.availHeight - this._paddleHeight, this._paddlePos);
+			this._paddlePos = Math.min(window.screen.availHeight - this._paddleHeight - BOTTOM_OFFSET,
+				this._paddlePos);
 			this._paddleSubject.next(this._paddlePos + "px");
 		}
 	}
